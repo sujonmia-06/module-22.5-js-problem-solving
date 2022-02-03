6bcc4a44ddf29e12f09abada3c873771b5ff5435
sujonmia <sujonmia.0612@gmail.com>
2022-02-03 19:18:22 +0600

    first commit

---
+++ p5.js
@@ -0,0 +1,21 @@
+/* ৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে। 
+
+ */
+
+let numbers = [38,926,53,15,37,85,33,-87,56,-08,89,-73,90,23,-4];
+
+function positiveNumbersArray(_array){
+    let positiveNumbers = []
+for (let number of numbers){
+    if (number >= 0){
+        positiveNumbers.push(number);
+    }
+    else{
+        break;
+    }
+}
+return positiveNumbers;
+}
+
+let filterPositiveNumbers = positiveNumbersArray(numbers);
+console.log(filterPositiveNumbers);
